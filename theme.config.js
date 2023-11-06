b7a34d8394c35fc3ddfac0b6b3c4c5e1ab6a31d0
heng.du <heng.du@ucloud.cn>
2023-11-06 13:49:35 +0800

    feat: add more posts

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © 小肚肚肚肚肚哦.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
