309eb20f000fb7bf03cea71ee9144536287cb690
heng.du <heng.du@ucloud.cn>
2023-11-29 19:14:14 +0800

    fix: change theme

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
-module.exports = withNextra();
+module.exports = withNextra({
+  darkMode: 'class'
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,33 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import { useEffect, useState } from 'react';
+
+import { DarkModeToggle } from '@anatoliygatt/dark-mode-toggle';
 
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
+
+  const [theme, setTheme] = useState('light');
+
+  const changeTheme = (isDark) => {
+    const html = document.querySelector('html');
+    if (html) {
+      html.classList.remove(['light']);
+      html.classList.remove(['dark']);
+      html.classList.add(isDark ? 'dark' : 'light');
+      html.style['colorScheme'] = isDark ? 'dark' : 'light';
+    }
+  }
+
+  useEffect(() => {
+    if (window) {
+      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      setTheme(isDark ? 'dark' : 'light');
+      changeTheme(isDark);
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -21,6 +45,29 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
+
+      <header style={{ float: 'right', marginTop: '8px' }}>
+        <DarkModeToggle
+          mode={theme}
+          dark="dark"
+          light="light"
+          size="sm"
+          inactiveTrackColor="#e2e8f0"
+          inactiveTrackColorOnHover="#f8fafc"
+          inactiveTrackColorOnActive="#cbd5e1"
+          activeTrackColor="#334155"
+          activeTrackColorOnHover="#1e293b"
+          activeTrackColorOnActive="#0f172a"
+          inactiveThumbColor="#1e293b"
+          activeThumbColor="#e2e8f0"
+          ariaLabel="Toggle color scheme"
+          onChange={(mode) => {
+            setTheme(mode);
+            changeTheme(mode === 'dark');
+          }}
+        />
+      </header>
+
       <Component {...pageProps} />
     </>
   )
