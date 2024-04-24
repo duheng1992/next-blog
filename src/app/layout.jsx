28f0178607b068b1928736cb7ea2624b7d8d9a68
heng.du <heng.du@ucloud.cn>
2024-04-24 11:17:17 +0800

    fix: change metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,8 +8,8 @@ import { HeroPattern } from '@/components/HeroPattern'
 
 export const metadata = {
   title: {
-    template: '%s - Protocol API Reference',
-    default: 'Protocol API Reference',
+    template: '%s - 小肚肚肚肚肚哦主页',
+    default: '小肚肚肚肚肚哦主页',
   },
 }
 
@@ -26,6 +26,14 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=2"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="小肚肚肚肚肚哦,小肚,前端,程序员,博客,blog,日常" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
         <Providers>
           <HeroPattern />
 
